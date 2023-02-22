dc2c85b28baa3406566944ad0c7fee75538b12f0
liveToRideRideToLive <mrk.dragicevic@gmail.com>
2023-02-22 11:28:23 +0100

    Updated selectors

--- test/pageobjects/secureLogoutPage.ts
+++ test/pageobjects/secureLogoutPage.ts
@@ -3,7 +3,7 @@ import Page from "./pageLogin";
 class SecurePageLogout extends Page {
    
     public get alertMsgLogout () {
-        return $(' .e8i0p5k0.errorText__3FGxxtgvq_ER-uskn2lqzd.gamut-1lo0dc4-Text');
+        return $("[data-testid='error-text']");
     }
 }
 
